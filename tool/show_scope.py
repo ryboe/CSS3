0db64cd2dd41665569b19ea106de326024ed72f2
y0ssar1an <ryanboehning@gmail.com>
2014-07-25 13:00:32 -0700

    move unncessary dev files from master to dev branch

--- tool/show_scope.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class PrintScopeNameCommand(sublime_plugin.EventListener):
-   def on_selection_modified(self, view):
-      sublime.status_message(view.scope_name(view.sel()[0].a))
