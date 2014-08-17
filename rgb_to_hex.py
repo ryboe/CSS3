36aca905055e089c31cccfac96b3cd0f5d3adbde
y0ssar1an <ryanboehning@gmail.com>
2014-08-16 17:47:18 -0700

    add missing prefixed properties suggested by thecotne. add completions for those properties. update property-list in /text with those properties. add rgb-to-hex color converter

---
+++ rgb_to_hex.py
@@ -0,0 +1,52 @@
+import sublime
+import sublime_plugin
+import urllib
+
+
+class HexConvertCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        window = self.view.window()
+        window.show_input_panel("RGB:", "", self.validate_and_convert, None, None)
+
+    def validate_and_convert(self, input_string):
+        input_string = "".join(input_string.split())  # remove all whitespace
+        if input_string == "":
+            print("the user entered nothing")
+            return
+
+        colors = input_string.split(",")
+        if len(colors) != 3:
+            sublime.error_message("ERROR: You entered {} colors. Three colors are required (red, "
+                                  "green, blue).\n\nHere's an example of a correct RGB value:\n\t"
+                                  "255, 255, 255".format(len(colors)))
+            return
+
+        color_values = []
+        try:
+            for color in colors:
+                color_value = int(color)
+                if not (0 <= color_value <= 255):
+                    raise ValueError(color)
+                color_values.append(color_value)
+        except ValueError as rgb_err:
+            sublime.error_message("ERROR: {} is outside the range 0-255".format(rgb_err))
+
+        hex_string = self.convert(color_values)
+
+        self.view.run_command("hex_insert", {"hex": hex_string})
+
+    def convert(self, rgb):
+        if all(c % 17 == 0 for c in rgb):
+            # return the three character hex notation, ex: #abc instead of #aabbcc
+            return "#" + "".join("{:x}".format(c)[0] for c in rgb)
+        return "#" + "".join("{:x}".format(c) for c in rgb)
+
+
+class HexInsertCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **args):
+        print("called")
+        for region in self.view.sel():
+            self.view.replace(edit, region, "")
+            insert_point = region.begin()
+            self.view.insert(edit, insert_point, args["hex"])
+
