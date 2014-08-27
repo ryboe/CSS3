88c0bf36559ad4a8736bd2dbeea716035b7a2bbe
y0ssar1an <ryanboehning@gmail.com>
2014-08-27 15:40:15 -0700

    add docstrings to rgb converter script

--- rgb_to_hex.py
+++ rgb_to_hex.py
@@ -4,11 +4,15 @@ import urllib
 
 
 class Css3HexConvertCommand(sublime_plugin.TextCommand):
+    """Convert RGB color values to hex colors"""
+
     def run(self, edit):
+        """Prompt the user for a comma-separated list of three numbers."""
         window = self.view.window()
         window.show_input_panel("RGB:", "", self.validate_and_convert, None, None)
 
     def validate_and_convert(self, input_string):
+        """Perform the conversion of RGB colors values to hex."""
         input_string = "".join(input_string.split())  # remove all whitespace
         if input_string == "":
             print("the user entered nothing")
@@ -37,6 +41,7 @@ class Css3HexConvertCommand(sublime_plugin.TextCommand):
         self.view.run_command("css3_hex_insert", {"hex": hex_string})
 
     def convert(self, rgb):
+        """Return a three-letter hex code if possible."""
         if all(c % 17 == 0 for c in rgb):
             # return the three character hex notation, ex: #abc instead of #aabbcc
             return "#" + "".join("{:x}".format(c)[0] for c in rgb)
@@ -44,9 +49,10 @@ class Css3HexConvertCommand(sublime_plugin.TextCommand):
 
 
 class Css3HexInsertCommand(sublime_plugin.TextCommand):
+    """Insert the hex colors from the converter into the text."""
+
     def run(self, edit, **args):
         for region in self.view.sel():
             self.view.replace(edit, region, "")
             insert_point = region.begin()
             self.view.insert(edit, insert_point, args["hex"])
-
