c8b19da7bb1c6396203316300a23c511640786a9
Ryan Boehning <ryan.boehning@apcera.com>
2016-06-30 16:28:24 -0700

    asdf

--- css3_completions.py
+++ css3_completions.py
@@ -12,21 +12,6 @@ property_name_rx = re.compile(r"(?P<prop_name>[-a-z0-9]+):")
 class CSS3Completions(sublime_plugin.EventListener):
 
     def on_query_completions(self, view, prefix, locations):
-<<<<<<< HEAD
-        trigger_start = locations[0] - len(prefix)
-
-        # Completions is a list of (<trigger>, <completion>) tuples.
-        # Completions are only offered for properties and values.
-        if view.match_selector(trigger_start, "meta.property-name.css"):
-            return properties.names, INHIBIT_DEFAULTS
-        elif view.match_selector(trigger_start, "meta.value.css"):
-            line = view.substr(view.line(trigger_start)).strip()
-            matches = property_name_rx.search(line)
-            if matches is not None:
-                prop_name = matches.group("prop_name")
-                if prop_name in properties.value_for_name:
-                    return properties.value_for_name[prop_name] + values.all_values, INHIBIT_DEFAULTS
-=======
         """Offer completions if they are available for the given cursor
         locations.
 
@@ -56,8 +41,3 @@ class CSS3Completions(sublime_plugin.EventListener):
                     prop_name = matches.group("prop_name")
                     if prop_name in properties.value_for_name:
                         return properties.value_for_name[prop_name] + values.all_values, INHIBIT_DEFAULTS
->>>>>>> new
-
-
-# TODO
-#   add completions for the scopes inside functions
--- tool/show_scope.py
+++ tool/show_scope.py
@@ -3,5 +3,6 @@ import sublime_plugin
 
 
 class PrintScopeNameCommand(sublime_plugin.EventListener):
+
     def on_selection_modified(self, view):
         sublime.status_message(view.scope_name(view.sel()[0].a))
