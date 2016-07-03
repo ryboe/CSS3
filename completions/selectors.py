7d36612427844e9af42256aeb03e3a183686ebb8
Ryan Boehning <ryan.boehning@apcera.com>
2016-07-03 15:38:27 -0700

    Add a bunch of completions

---
+++ completions/selectors.py
@@ -0,0 +1,32 @@
+at_page_selectors = [
+    ("@bottom-center", "@bottom-center {\n\t${1}\n}"),
+    ("@bottom-left", "@bottom-left {\n\t${1}\n}"),
+    ("@bottom-left-corner", "@bottom-left-corner {\n\t${1}\n}"),
+    ("@bottom-right", "@bottom-right {\n\t${1}\n}"),
+    ("@bottom-right-corner", "@bottom-right-corner {\n\t${1}\n}"),
+    ("@left-bottom", "@left-bottom {\n\t${1}\n}"),
+    ("@left-middle", "@left-middle {\n\t${1}\n}"),
+    ("@left-top", "@left-top {\n\t${1}\n}"),
+    ("@right-bottom", "@right-bottom {\n\t${1}\n}"),
+    ("@right-middle", "@right-middle {\n\t${1}\n}"),
+    ("@right-top", "@right-top {\n\t${1}\n}"),
+    ("@top-center", "@top-center {\n\t${1}\n}"),
+    ("@top-left", "@top-left {\n\t${1}\n}"),
+    ("@top-left-corner", "@top-left-corner {\n\t${1}\n}"),
+    ("@top-right", "@top-right {\n\t${1}\n}"),
+    ("@top-right-corner", "@top-right-corner {\n\t${1}\n}"),
+]
+
+font_feature_types = [
+    ("@annotation", "@annotation {\n\t${1}\n}"),
+    ("@character-variant", "@character-variant {\n\t${1}\n}"),
+    ("@ornaments", "@ornaments {\n\t${1}\n}"),
+    ("@styleset", "@styleset {\n\t${1}\n}"),
+    ("@stylistic", "@stylistic {\n\t${1}\n}"),
+    ("@swash", "@swash {\n\t${1}\n}"),
+]
+
+keyframes_selector = [
+    ("from", "from {\n\t${1}\n}"),
+    ("to", "to {\n\t${1}\n}"),
+]
