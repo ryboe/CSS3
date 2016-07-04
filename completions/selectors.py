7317539cd192b87eb207184fa41f42e873689bd5
Ryan Boehning <ryan.boehning@apcera.com>
2016-07-03 18:44:14 -0700

    Add more completions

--- completions/selectors.py
+++ completions/selectors.py
@@ -1,4 +1,31 @@
+# @-rules that can appear inside other @-rules.
+nestable_at_rules = [
+    ("@counter-style", "@counter-style ${1:name} {\n\t${2}\n}"),
+    ("@font-face", "@font-face {\n\t${1}\n}"),
+    ("@font-feature-values", "@font-feature-values ${1:font-family} {\n\t${2}\n}"),
+    ("@keyframes", "@keyframes ${1:name} {\n\t${2}\n}"),
+    ("@media", "@media ${1:media-query-list} {\n\t${2}\n}"),
+    ("@page", "@page ${1} {\n\t${2}\n}"),
+    ("@viewport", "@viewport ${1} {\n\t${2}\n}"),
+]
+
+at_rules = [
+    ("@charset", '@charset "UTF-8";'),
+    ("@color-profile", "@color-profile {\n\t${2}\n}"),
+    ("@custom-media", "@custom-media --${1:name} ${2:media-query-list};"),
+    ("@import", "@import ${1:path} ${2:media-query-list};"),
+    ("@namespace", "@namespace ${1};"),
+    ("@supports", "@supports ${1} {\n\t${2}\n}"),
+] + nestable_at_rules
+
 at_page_selectors = [
+    (":blank",),
+    (":first",),
+    (":left",),
+    (":right",),
+]
+
+page_margin_boxes = [
     ("@bottom-center", "@bottom-center {\n\t${1}\n}"),
     ("@bottom-left", "@bottom-left {\n\t${1}\n}"),
     ("@bottom-left-corner", "@bottom-left-corner {\n\t${1}\n}"),
@@ -30,3 +57,5 @@ keyframes_selector = [
     ("from", "from {\n\t${1}\n}"),
     ("to", "to {\n\t${1}\n}"),
 ]
+
+at_rules.sort()
