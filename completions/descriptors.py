7ec50b7d446d97ba4b1e5a4599f737cb3a4eba6f
y0ssar1an <ryanboehning@gmail.com>
2016-07-04 22:05:07 -0700

    Split descriptors into separate file. Add properties

---
+++ completions/descriptors.py
@@ -0,0 +1,37 @@
+# DESCRIPTORS
+color_profile_descriptors = [
+    ("name", "name: ${1};"),
+    ("rendering-intent", "rendering-intent: ${1};"),
+    ("src", "src: ${1};"),
+]
+
+counter_style_descriptors = [
+    ("additive-symbols", "additive-symbols: ${1};"),
+    ("fallback", "fallback: ${1};"),
+    ("negative", "negative: ${1};"),
+    ("pad", "pad: ${1};"),
+    ("prefix", "prefix: ${1};"),
+    ("range", "range: ${1};"),
+    ("speak-as", "speak-as: ${1};"),
+    ("suffix", "suffix: ${1};"),
+    ("symbols", "symbols: ${1};"),
+    ("system", "system: ${1};"),
+]
+
+font_face_descriptors = [
+    ("font-family", "font-family: ${1};"),
+    ("font-feature-settings", "font-feature-settings: ${1};"),
+    ("font-stretch", "font-stretch: ${1};"),
+    ("font-style", "font-style: ${1};"),
+    ("font-variant", "font-variant: ${1};"),
+    ("font-weight", "font-weight: ${1};"),
+    ("src", "src: ${1};"),
+]
+
+viewport_descriptors = [
+    ("height", "height: ${1};"),
+    ("orientation", "orientation: ${1};"),
+    ("user-zoom", "user-zoom: ${1};"),
+    ("width", "width: ${1};"),
+    ("zoom", "zoom: ${1};"),
+]
